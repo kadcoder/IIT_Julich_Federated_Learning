6f8e99bc09fbdab0d49a39908b2129e076369e5a
Nicolás Nieto <nnieto@sinc.unl.edu.ar>
2025-02-25 15:12:13 +0100

    Add comments in  eval.py. Please check it

--- utils/eval.py
+++ utils/eval.py
@@ -11,11 +11,11 @@ def evaluate_model(test_path, model_path):
     # Load the model (assuming model_name corresponds to a model function or class)
 
     model = m.AgePredictor(X_test.shape[1])
-    model.load_state_dict(torch.load(model_path, map_location=config.DEVICE))
+    model.load_state_dict(torch.load(model_path, map_location=config.DEVICE))            # This looks quite subobtimal. Do you need to save the model every time that you evaluate it?. It makes more sense that the trained object is passed to the function
     model.eval()
 
     scaler = StandardScaler()
-    X_test = scaler.fit_transform(X_test)
+    X_test = scaler.fit_transform(X_test)    # This is wrong, you have to use the already trained standard scaler on the train data. You should pass the trained object to the function.
 
     # Convert to tensors
     test_loader = utils.dataloader(X_test,y_test)
